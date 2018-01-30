a841006a1c8c4423799843382c6b2f0aa44bd215
Kate Birmingham <kabi6686@colorado.edu>
2018-01-29 23:24:00 -0700

    have functional screen flow and styling, still need game function and question display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,164 @@
 // On page load:
 $(function () {
+	//variable for userPick
+	var userPick;
 	//variable for correctAnswer
 	var correctAnswer = 0;
 	//variable for incorrectAnswer
-	var incorrectAnswers = 0;
+	var incorrectAnswer = 0;
 	//varialbe for unansweredQuestion
-	var unansweredQuestion = 0;
+	var unAnswer = 0;
 	//variable for counter
-	var counter = 0;
+	var counter = 11;
+	//need variable for intervalID
+	var intervalId;
 	//need a variable for userGuess
-	var userGuess = ["Answer1", "Answer2", "Answer3", "Answer4"];
-	//array of rightAnswer
-	var rightAnswer = ["Answer1", "Answer2", "Answer3", "Answer4", "Answer5"];
-	// array of answers
-	var answersArr= {
-		Answers1: 
-		Answers2:
-		Answers3:
-		Answers4:
-		Answers5:
-	
-	}
-	//array of questions
-	var questionsArr= [ {
-
-		Question1: "What is the corpos callosum?",
-		Question2: "What other bodily systems does the nervous system directly interact?",
-		Question3:
-		Question4:
-		Question5:
+	var scienceQuestion = [ {
+		question: "What is the corpus callosum?",
+		choices: ["A sweet word with no function", "A type of snail", "A receptor", "It connects the left and right brain hemispheres"],
+		validAnswer: 3
+	}, {
+		question: "What bodily system does the nervous system directly interact with?",
+		choices: ["Digestive System", "Immune System", "Reproductive System", "All of the above"],
+		validAnswer: 3
+	}, {
+		question: "What is NOT a neurotransmitter",
+		choices: ["Serotonin", "Nitric Oxide", "Insula", "Acetylcholine"],
+		validAnswer: 2
+	}, {
+		question: "What drug is responsible for interacting with endocannabinoids?",
+		choices: ["Marijuana", "Heroin", "LSD", "Cocaine"],
+		validAnswer: 0
+	}, {
+		question: "What molecule is increased in the synapse when you are sleepy?",
+		choices: ["Caffeine", "Adenosine", "Glutamate", "Dopamine"],
+		validAnswer: 1
+	}, {
+		question: "What is scientifically proven to help prevent or help resistance to stress?",
+		choices: ["Placebo", "Exercise", "Midnight snacks", "Medication"],
+		validAnswer: 1
 	}
 
 	];
-	//reset function
-		// set correctAnswers to 0
-		// set incorrectAnswers to 0
-		// set unansweredQuestion to 0
-		// set counter to 120 seconds
-	//update function
-		//displays correct answers
-		//displays incorrect answers
-		//displays unanswered questions
-	//start at start screen
-		//Click Start Button:
-			//move to game screen
-				//counter starts
-				//counter decreases by 1
-				//user clicks button for each question
-				//correct answer=specific button
-		//counter = 0 seconds:
-			//move to stop screen
-				//if user guesses=correctAnswer,
-					//increase correctAnswers by 1
-					//call update
-				// else if userGuess!=correctAnswer
-					//increase incorrect Answer by 1
-					//call update
-				// else if userGuess=empty
-					//increase unanswered by 1
-					//call update
 
-		//click start over button
-			//return to start screen
-			//call reset
+	//reset function
+	function reset() {
+		// set correctAnswers to 0
+		correctAnswer = 0;
+		// set incorrectAnswers to 0
+		incorrectAnswer = 0;
+		// set unansweredQuestion to 0
+		unAnswer = 0;
+		// set counter to 120 seconds
+		counter = 11;
+		//clear intervalId
+		clearInterval(intervalId);
+		$("#questionDiv").empty();
+		$("#choicesDiv").empty();
+		//reset trivia
+	} // END RESET FUNCTION
+
+	reset();
+
+	//update function
+	function update() {
+		//displays correct answers
+		$("#correct").html("Correct Answers: " + correctAnswer + "/5");
+		//displays incorrect answers
+		$("#incorrect").html("Incorrect Answers: " + incorrectAnswer + "/5");
+		//displays unanswered questions
+		$("#unanswered").html("Unanswered Questions: " + unAnswer + "/5");
+		//create loop to display questions 
+
+	} // END UPDATE FUNCTION
+
+	update();
+
+	function run() {
+
+		clearInterval(intervalId);
+		intervalId = setInterval(decrement, 1000);
+	}
+
+	function decrement () {
+		counter--;
+		if (counter <= 0) {
+			clearInterval(intervalId);
+			$(".stopScreen").show();
+			$(".gameScreen").hide();
+		}
+		$(".timeTracker").html("Time Remaining: " + counter + " seconds");
+	}
+
+	function displayTrivia() {
+
+		var newDiv = $("<div>");
+		newDiv.append(scienceQuestion[0].question);
+		$("#questionDiv").append(newDiv);
+
+		var choicesArr = scienceQuestion[0].choices;
+
+		for (var i = 0; i < choicesArr.length; i++) {
+			var newDiv = $("<div>");
+			newDiv.text(choicesArr[i]);
+			$("#choicesDiv").append(newDiv);
+			// var button = $("<button>");
+			// button.addclass(".btn-circle");
+
+
+		}
+		
+	}
+
+	//start at start screen:
+
+	$(".gameScreen").hide();
+	
+	$(".stopScreen").hide();
+
+	$(".startScreen").show();
+
+	//Click Start Button:
+	$(".start").on("click", function() {
+		//move to game screen
+		$(".gameScreen").show();
+		//remove start button
+		$(".startScreen").hide();
+		//counter starts
+		run();
+		// counter decreases by one
+		decrement();
+		//put questions on page
+		displayTrivia();
+
+		}); // END START BUTTON
+
+		//Click an answer button
+	$(".btn-circle").on("click", function() {
+			userPick = $(this).data("id");
+			scienceQuestin[0].validAnswer;
+
+		if (userPick === scienceQuestion[0].validAnswer) {
+			correctAnswer++;
+		} else if (userPick != scienceQuestion[0].validAnswer) {
+			incorrectAnswer++;
+		} else if (userPick == "") {
+			unAnswer++;
+		}
+				
+
+		}); // END CLICK CIRCLE BUTTON
+
+	//click start over button
+	$(".start-over").on("click", function (){ 
+		//return to start screen
+		$(".startScreen").show();
+		$(".gameScreen").hide();
+		$(".stopScreen").hide();
+		//call reset
+		reset();
+			
+	}); // END START OVER BUTTON
+
 
 }); // END PAGE LOAD
